d137293164532ce5127fc86f765b701740b5cc14
Pernilla Sterner <psterner@hotmail.com>
2024-03-20 21:08:49 +0100

    Make use of state

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+// import AppClass from './components/AppClass';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    {/* <AppClass /> */}
   </React.StrictMode>
 );
 
